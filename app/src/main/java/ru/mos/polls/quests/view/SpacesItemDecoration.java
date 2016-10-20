312188f6e0ff4c36f2676633534d3375e8e92d58
Ling <vivalatrunks@gmail.com>
2016-10-20 17:07:18 +0300

    update CircleImageView

--- app/src/main/java/ru/mos/polls/quests/view/SpacesItemDecoration.java
+++ app/src/main/java/ru/mos/polls/quests/view/SpacesItemDecoration.java
@@ -18,6 +18,6 @@ public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
         outRect.right = space;
         outRect.bottom = space;
         if (parent.getChildAdapterPosition(view) == 0)
-            outRect.top = 0;
+            outRect.top = 90;
     }
 }

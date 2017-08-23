9135082ea9029e971cebff23bb09893332ec1192
Ling <vivalatrunks@gmail.com>
2017-08-23 18:13:05 +0300

    переделка мои баллы

---
+++ app/src/main/java/ru/mos/polls/mypoints/ui/NewMyPointsFragment.java
@@ -0,0 +1,8 @@
+package ru.mos.polls.mypoints.ui;
+
+/**
+ * Created by Trunks on 23.08.2017.
+ */
+
+public class NewMyPointsFragment {
+}
---
+++ app/src/main/java/ru/mos/polls/mypoints/vm/NewMyPointsFragmentVM.java
@@ -0,0 +1,8 @@
+package ru.mos.polls.mypoints.vm;
+
+/**
+ * Created by Trunks on 23.08.2017.
+ */
+
+public class NewMyPointsFragmentVM {
+}

191b47ec0480f1b642c33ef6d353714e129f706a
matek3022 <m-semenov30@yandex.ru>
2017-09-21 15:43:35 +0400

    добавлены лайауты и сущности, часть логики пока работает не корректно

---
+++ app/src/main/java/ru/mos/polls/newabout/state/AboutAppState.java
@@ -0,0 +1,49 @@
+package ru.mos.polls.newabout.state;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+
+import me.ilich.juggler.gui.JugglerFragment;
+import me.ilich.juggler.states.ContentToolbarNavigationState;
+import me.ilich.juggler.states.VoidParams;
+import ru.mos.polls.R;
+import ru.mos.polls.base.ui.CommonToolbarFragment;
+import ru.mos.polls.base.ui.NavigationFragmentJuggler;
+import ru.mos.polls.newabout.ui.fragment.AboutAppFragment;
+
+/**
+ * Created by matek3022 on 21.09.17.
+ */
+
+public class AboutAppState extends ContentToolbarNavigationState<VoidParams> {
+
+    public AboutAppState(VoidParams params) {
+        super(params);
+    }
+
+    @Override
+    public String getTitle(Context context, VoidParams params) {
+        return context.getString(R.string.title_help);
+    }
+
+    @Override
+    public Drawable getUpNavigationIcon(Context context, VoidParams params) {
+        return context.getResources().getDrawable(R.drawable.nb_icon_navbar);
+    }
+
+    @Override
+    protected JugglerFragment onConvertContent(VoidParams params, @Nullable JugglerFragment fragment) {
+        return AboutAppFragment.instance(null);
+    }
+
+    @Override
+    protected JugglerFragment onConvertToolbar(VoidParams params, @Nullable JugglerFragment fragment) {
+        return CommonToolbarFragment.createNavigation();
+    }
+
+    @Override
+    protected JugglerFragment onConvertNavigation(VoidParams params, @Nullable JugglerFragment fragment) {
+        return NavigationFragmentJuggler.create(R.id.menu_support);
+    }
+}
---
+++ app/src/main/java/ru/mos/polls/newabout/vm/AboutAppFragmentVM.java
@@ -0,0 +1,130 @@
+package ru.mos.polls.newabout.vm;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+
+import me.ilich.juggler.change.Add;
+import me.ilich.juggler.states.VoidParams;
+import ru.mos.elk.BaseActivity;
+import ru.mos.elk.api.API;
+import ru.mos.polls.BuildConfig;
+import ru.mos.polls.GoogleStatistics;
+import ru.mos.polls.R;
+import ru.mos.polls.Statistics;
+import ru.mos.polls.ToolbarDynamicActivity;
+import ru.mos.polls.UrlManager;
+import ru.mos.polls.WebViewActivity;
+import ru.mos.polls.base.component.RecyclerUIComponent;
+import ru.mos.polls.base.component.UIComponentFragmentViewModel;
+import ru.mos.polls.base.component.UIComponentHolder;
+import ru.mos.polls.databinding.LayoutAboutAppBinding;
+import ru.mos.polls.fragments.DynamicFragment;
+import ru.mos.polls.helpers.FunctionalHelper;
+import ru.mos.polls.instruction.InstructionActivity;
+import ru.mos.polls.newabout.model.AboutItem;
+import ru.mos.polls.newabout.ui.fragment.AboutAppFragment;
+import ru.mos.polls.newsupport.state.SupportState;
+import ru.mos.polls.social.controller.SocialUIController;
+import ru.mos.polls.social.model.AppPostValue;
+
+/**
+ * Created by matek3022 on 21.09.17.
+ */
+
+public class AboutAppFragmentVM extends UIComponentFragmentViewModel<AboutAppFragment, LayoutAboutAppBinding> {
+    private SocialListener socialListener;
+
+    public AboutAppFragmentVM(AboutAppFragment fragment, LayoutAboutAppBinding binding, SocialListener socialListener) {
+        super(fragment, binding);
+        this.socialListener = socialListener;
+    }
+
+    @Override
+    protected void initialize(LayoutAboutAppBinding binding) {
+        Statistics.appsDescription();
+        GoogleStatistics.AGNavigation.appsDescription();
+    }
+
+    @Override
+    protected UIComponentHolder createComponentHolder() {
+        return new UIComponentHolder.Builder()
+                .with(new RecyclerUIComponent<RecyclerView.Adapter>(AboutItem.getAdapter((id, position) -> {
+                    Intent intent = null;
+                    switch (id) {
+                        case AboutItem.ABOUT_PROJECT:
+                            WebViewActivity.startActivity(getActivity(),
+                                    getActivity().getString(R.string.title_about_project),
+                                    getActivity().getString(R.string.url_about_app),
+                                    null,
+                                    false,
+                                    false);
+                            break;
+                        case AboutItem.USER_GUIDE:
+                            WebViewActivity.startActivity(getActivity(),
+                                    getActivity().getString(R.string.title_user_guide),
+                                    getActivity().getString(R.string.url_user_guide),
+                                    null,
+                                    false,
+                                    false);
+                            break;
+                        case AboutItem.INSTRUCTION:
+                            InstructionActivity.startActivity(getActivity());
+                            break;
+                        case AboutItem.OFFER:
+                            WebViewActivity.startActivity(getActivity(),
+                                    getActivity().getString(R.string.title_offer),
+                                    getActivity().getString(R.string.url_offer),
+                                    null,
+                                    false,
+                                    false);
+                            break;
+                        case AboutItem.OUR_APPS:
+                            Statistics.ourApps();
+                            GoogleStatistics.AGNavigation.ourApps();
+                            intent = new Intent(getActivity(), ToolbarDynamicActivity.class);
+                            intent.putExtra(DynamicFragment.BASE_URL, API.getURL(UrlManager.url(UrlManager.Controller.INFORAMTION, UrlManager.Methods.GET_OUR_APPS)));
+                            intent.putExtra(DynamicFragment.PARAMS, "{\"user_agent\": \"android\"}");
+                            intent.putExtra(DynamicFragment.DEF_TITLE, getActivity().getString(R.string.our_apps));
+                            getActivity().startActivity(intent);
+                            break;
+                        case AboutItem.SHARE_SOCIAL:
+                            SocialUIController.showSocialsDialog((BaseActivity) getActivity(), new SocialUIController.SocialClickListener() {
+                                @Override
+                                public void onClick(Context context, Dialog dialog, AppPostValue appPostValue) {
+                                    socialListener.onSocialPost(appPostValue);
+                                }
+
+                                @Override
+                                public void onCancel() {
+                                    getActivity().finish();
+                                }
+                            });
+                            break;
+                        case AboutItem.RATE_APP:
+                            FunctionalHelper.startGooglePlay(getActivity());
+                            break;
+                        case AboutItem.FEEDBACK:
+//                        AgSupportActivity.startActivity(getActivity());
+                            ((ru.mos.polls.base.ui.BaseActivity)getActivity()).navigateTo().state(Add.newActivity(new SupportState(VoidParams.instance()), ru.mos.polls.base.ui.BaseActivity.class));
+                            break;
+                    }
+                })))
+                .build();
+    }
+
+    @Override
+    public void onViewCreated() {
+        super.onViewCreated();
+        getBinding().appVersion.setText(getActivity().getString(R.string.app_version_template, BuildConfig.VERSION_NAME));
+    }
+
+    public void setSocialListener(SocialListener socialListener) {
+        this.socialListener = socialListener;
+    }
+
+    public static interface SocialListener {
+        void onSocialPost(AppPostValue appPostValue);
+    }
+}

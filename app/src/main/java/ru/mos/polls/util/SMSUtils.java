9e54a20144299ded1be4725a83a25cc1fb568e8b
Matvey.Semenov <m-semenov30@yandex.ru>
2017-09-28 15:24:18 +0400

    refs #96536 - сделана отправка смс с подтверждением об успешной доставке

--- app/src/main/java/ru/mos/polls/util/SMSUtils.java
+++ app/src/main/java/ru/mos/polls/util/SMSUtils.java
@@ -1,7 +1,11 @@
 package ru.mos.polls.util;
 
+import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.telephony.SmsManager;
 
 import java.util.ArrayList;
@@ -14,6 +18,25 @@ import ru.mos.polls.R;
  */
 
 public class SMSUtils {
+
+    private static final String DELIVER_RECEIVER_FILTER = "ru.mos.polls.util.deliver_receiver_filter";
+    private static final String SENDING_MESSAGE_KEY = "ru.mos.polls.util.sending_message_key";
+
+    private static final BroadcastReceiver deliverReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String msg = intent.getStringExtra(SENDING_MESSAGE_KEY);
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    GuiUtils.displayOkMessage(context, String.format(context.getString(R.string.success_send_sms), msg), null);
+                    break;
+                default:
+                    GuiUtils.displayOkMessage(context, String.format(context.getString(R.string.error_send_sms), msg), null);
+            }
+            context.unregisterReceiver(deliverReceiver);
+        }
+    };
+
     public static void sendSMS(Context context, String phoneNumber, String[] text) {
         ArrayList<String> smsBody = new ArrayList<>();
         Collections.addAll(smsBody, text);
@@ -26,15 +49,21 @@ public class SMSUtils {
             stringText += text.get(i);
             if (i != text.size() - 1) stringText += "\n";
         }
+        Intent messageIntent = new Intent(DELIVER_RECEIVER_FILTER);
+        messageIntent.putExtra(SENDING_MESSAGE_KEY, stringText);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(context, 0, messageIntent, 0);
+        ArrayList<PendingIntent> list = new ArrayList<>();
+        list.add(deliveredPI);
+        context.registerReceiver(deliverReceiver, new IntentFilter(DELIVER_RECEIVER_FILTER));
+        SmsManager sms = SmsManager.getDefault();
         try {
-            SmsManager sms = SmsManager.getDefault();
             sms.sendMultipartTextMessage(phoneNumber,
                     null,
                     text,
                     new ArrayList<PendingIntent>(),
-                    new ArrayList<PendingIntent>());
-            GuiUtils.displayOkMessage(context, String.format(context.getString(R.string.success_send_sms), stringText), null);
-        } catch (Exception e) {
+                    list);
+        }catch (Exception e) {
+            context.unregisterReceiver(deliverReceiver);
             GuiUtils.displayOkMessage(context, String.format(context.getString(R.string.error_send_sms), stringText), null);
         }
     }

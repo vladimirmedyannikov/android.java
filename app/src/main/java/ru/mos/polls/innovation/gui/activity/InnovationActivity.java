252240054ba896c471849b4773654c0ae93ff2ad
Matvey.Semenov <m-semenov30@yandex.ru>
2017-10-24 13:41:44 +0400

    refs #97628 - починил

--- app/src/main/java/ru/mos/polls/innovation/gui/activity/InnovationActivity.java
+++ app/src/main/java/ru/mos/polls/innovation/gui/activity/InnovationActivity.java
@@ -328,8 +328,9 @@ public class InnovationActivity extends ToolbarAbstractActivity implements Innov
     private String getDialogMessage(int point, int allPoints) {
         String message;
         if (point > 0) {
-            String pointTxt = PointsManager.getPointUnitString(this, point);
-            message = String.format(getString(R.string.novelty_result_send_share_with_points), String.valueOf(point), pointTxt, String.valueOf(allPoints), PointsManager.getPointUnitString(this, allPoints));
+//            String pointTxt = PointsManager.getPointUnitString(this, point);
+//            message = String.format(getString(R.string.novelty_result_send_share_with_points), String.valueOf(point), pointTxt, String.valueOf(allPoints), PointsManager.getPointUnitString(this, allPoints));
+            message = PointsManager.getMessage(this, point, allPoints);
         } else {
             message = getString(R.string.novelty_result_send_share);
         }

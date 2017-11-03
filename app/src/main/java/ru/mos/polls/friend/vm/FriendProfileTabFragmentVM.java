0a1fa34132df204901f788b6685ceec082a37b11
Ling <vivalatrunks@gmail.com>
2017-11-03 15:48:08 +0300

    правка заголовка профиля друга

--- app/src/main/java/ru/mos/polls/friend/vm/FriendProfileTabFragmentVM.java
+++ app/src/main/java/ru/mos/polls/friend/vm/FriendProfileTabFragmentVM.java
@@ -95,27 +95,7 @@ public class FriendProfileTabFragmentVM extends UIComponentFragmentViewModel<Fri
                     .getTabAt(index)
                     .setIcon(pages.get(index).getIconResId());
         }
-        slidingTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                switch (tab.getPosition()) {
-                    case 0:
-                        getFragment().getActivity().setTitle(R.string.profile_title);
-                        break;
-                    case 1:
-                        getFragment().getActivity().setTitle(R.string.achievements);
-                        break;
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
+        getFragment().getActivity().setTitle(R.string.profile_title);
         subscribeEventsBus();
     }
 

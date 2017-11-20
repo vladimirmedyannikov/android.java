3b2e2cf8c1fb5cfa2c8251654c8a7925848099e5
wlTrunks <vivalatrunks@gmail.com>
2017-11-20 18:03:37 +0300

    test

---
+++ app/src/androidTest/java/ru/mos/polls/sourcesvoting/SourcesVotingUnitTest.java
@@ -0,0 +1,23 @@
+package ru.mos.polls.sourcesvoting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.mos.polls.BaseUnitTest;
+import ru.mos.polls.sourcesvoting.model.SourcesVoting;
+
+/**
+ * Created by wlTrunks on 20.11.2017.
+ */
+
+public class SourcesVotingUnitTest extends BaseUnitTest {
+
+    @Test
+    public void gettersTest() {
+        SourcesVoting votingSet = mockObj("sourcesvoting.json", SourcesVoting.class);
+        Assert.assertNotEquals(votingSet.getId(), 0);
+        Assert.assertNotNull(votingSet.getTitle());
+        Assert.assertTrue(votingSet.isEditable());
+        Assert.assertTrue(votingSet.isEnable());
+    }
+}

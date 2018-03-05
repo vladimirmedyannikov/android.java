27a62a075426cf0671d2d60668a763f874fbfe6f
Matvey.Semenov <m-semenov30@yandex.ru>
2018-03-05 18:18:31 +0400

    переписан UserInfoAdapter в InfoTabFragment

--- app/src/main/java/ru/mos/polls/profile/ui/adapter/UserInfoAdapter.java
+++ app/src/main/java/ru/mos/polls/profile/ui/adapter/UserInfoAdapter.java
@@ -2,44 +2,29 @@ package ru.mos.polls.profile.ui.adapter;
 
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import ru.mos.polls.R;
-import ru.mos.polls.base.ui.BindingHolder;
-import ru.mos.polls.base.ui.adapter.BaseAdapter;
-import ru.mos.polls.databinding.UserInfoItemBinding;
+import ru.mos.polls.base.BaseRecyclerAdapter;
 import ru.mos.polls.profile.model.UserInfo;
 import ru.mos.polls.profile.vm.UserInfoVM;
 
-public class UserInfoAdapter extends BaseAdapter<UserInfoVM, BindingHolder<UserInfoItemBinding>, UserInfoItemBinding, UserInfo> {
+public class UserInfoAdapter extends BaseRecyclerAdapter<UserInfoVM> {
 
     private View.OnClickListener onItemClickListener;
 
     public UserInfoAdapter(View.OnClickListener onItemClickListener, List<UserInfo> list) {
         this.onItemClickListener = onItemClickListener;
-        this.list = list;
+        add(list);
     }
 
-    @Override
-    public BindingHolder<UserInfoItemBinding> getVH(UserInfoItemBinding binding) {
-        return new BindingHolder<>(binding);
-    }
-
-    @Override
-    public UserInfoVM getVM(UserInfo obj, UserInfoItemBinding binding) {
-        return new UserInfoVM(obj, binding);
-    }
-
-    @Override
-    public int getLayoutResources() {
-        return R.layout.user_info_item;
-    }
-
-    @Override
-    public void onBindViewHolder(BindingHolder<UserInfoItemBinding> holder, int position) {
-        super.onBindViewHolder(holder, position);
-        if (position != 0) {
-            holder.itemView.setOnClickListener(onItemClickListener);
+    public void add(List<UserInfo> list) {
+        List<UserInfoVM> content = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            UserInfo item = list.get(i);
+            UserInfoVM userInfoVM = new UserInfoVM(item, i != 0 ?onItemClickListener : null);
+            content.add(userInfoVM);
         }
+        addData(content);
     }
 }
--- app/src/main/java/ru/mos/polls/profile/vm/UserInfoVM.java
+++ app/src/main/java/ru/mos/polls/profile/vm/UserInfoVM.java
@@ -1,14 +1,40 @@
 package ru.mos.polls.profile.vm;
 
 
-import ru.mos.polls.base.vm.BaseVM;
+import android.view.View;
+
+import ru.mos.polls.R;
+import ru.mos.polls.base.RecyclerBaseViewModel;
 import ru.mos.polls.databinding.UserInfoItemBinding;
 import ru.mos.polls.profile.model.UserInfo;
 
-public class UserInfoVM extends BaseVM<UserInfo, UserInfoItemBinding> {
+public class UserInfoVM extends RecyclerBaseViewModel<UserInfo, UserInfoItemBinding> {
 
-    public UserInfoVM(UserInfo userData, UserInfoItemBinding binding) {
-        super(userData, binding);
+    private View.OnClickListener onItemClickListener;
+
+    public UserInfoVM(UserInfo userData, View.OnClickListener onItemClickListener) {
+        super(userData);
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @Override
+    public void onBind(UserInfoItemBinding viewDataBinding) {
+        super.onBind(viewDataBinding);
+        viewDataBinding.setViewModel(this);
+        viewDataBinding.executePendingBindings();
+        if (onItemClickListener != null) {
+            viewDataBinding.root.setOnClickListener(onItemClickListener);
+        }
+    }
+
+    @Override
+    public int getViewType() {
+        return 0;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.user_info_item;
     }
 
     public String getValue() {
